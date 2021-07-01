55b91a6ed86b38568ef38e640612f8a66e40a803
Jason McKenzie <jmac.atx@gmail.com>
2021-07-01 10:47:01 -0500

    fixes edits to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,7 @@ class UsersController < ApplicationController
   end
 end
 
-# def create
-#   user = User.new(
-#     name: params[:name],
-#     email: params[:email],
-#     password: params[:password],
-#     password_confirmation: params[:password_confirmation]
-#   )
-# if user.save
-#   render json: { message: "User created successfully" }, status: :created
-# else
-#   render json: { errors: user.errors.full_messages }, status: :bad_request
-# end
-# end
+def show
+  user = User.find(params[:id])
+  render json: user
+end
